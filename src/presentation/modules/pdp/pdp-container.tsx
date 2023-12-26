10991e79bb39d73c30e8c96b4861d0e48eefc753
Damián Cancela <damian.cancela@cencosud.com>
2023-12-26 21:29:38 +0000

    EASY-4810/ feat: made pdp ssg

--- src/presentation/modules/pdp/pdp-container.tsx
+++ src/presentation/modules/pdp/pdp-container.tsx
@@ -1,17 +1,15 @@
 import React, { useEffect } from 'react';
-import { useQuery } from 'react-query';
-import dynamic from 'next/dynamic';
-import { useAppDispatch } from '@hooks/storeHooks';
-import getProduct from '@use-cases/product/get-product';
+// import dynamic from 'next/dynamic';
+import { useAppDispatch, useAppSelector } from '@hooks/storeHooks';
 import { setProduct } from '@store/product';
 import ImageGallery from './sections/image-gallery';
 import ProductDetails from './sections/product-details';
-import { PdpProps } from './types';
 import {
   DetailsContainer,
   ImagesContainer,
   Main,
   ReviewContainer,
+  // ReviewContainer,
   Section,
   Separator,
 } from './style';
@@ -21,8 +19,10 @@ import SpecificationsTables from './sections/specifications-tables';
 import { RatingsProps } from '@entities/ratings-and-reviews/ratings-and-reviews.type';
 import { GetProduct } from '@entities/product/get-product.response';
 import FirstLoadSkeleton from '@components/molecules/skeleton/ratings-and-reviews/FirstLoadSkeleton';
-import PdpSkeleton from '@components/molecules/pdp-skeleton/pdp-skeleton';
 import { setImages } from '@store/gallery';
+import CartEventProvider from '../../providers/cart-event-provider';
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
 
 const RatingAndReview = dynamic<RatingsProps>(
   () => import('ratingsAndReviews/index'),
@@ -32,48 +32,46 @@ const RatingAndReview = dynamic<RatingsProps>(
   },
 );
 
-const PdpContainer = ({ productId }: PdpProps) => {
+const PdpContainer = (productData: GetProduct) => {
+  const { product } = useAppSelector((state) => state.product);
   const dispatch = useAppDispatch();
 
-  const { data, isLoading, isError } = useQuery(
-    ['get-product', productId],
-    () => {
-      if (productId) return getProduct(productId);
-    },
-    { enabled: Boolean(productId) },
-  );
-
   useEffect(() => {
-    if (data) {
-      dispatch(setProduct(data));
-      dispatch(setImages(data?.items?.[0].images));
+    if (productData) {
+      dispatch(setProduct(productData));
+      dispatch(setImages(productData?.items?.[0].images));
     }
-  }, [data, dispatch]);
-
-  if (isLoading) return <PdpSkeleton />;
-
-  if (isError) return <PdpSkeleton />;
+  }, [productData, dispatch]);
 
   return (
-    <Main>
-      <PdpBreadcrumbs />
-      <Section>
-        <ImagesContainer>
-          <ImageGallery />
-        </ImagesContainer>
-        <Separator />
-        <DetailsContainer>
-          <ProductDetails />
-        </DetailsContainer>
-      </Section>
-      <EmotionalDescription />
-      <Section id="specifications">
-        <SpecificationsTables />
-      </Section>
-      <ReviewContainer>
-        {data && <RatingAndReview productInfo={data as GetProduct} />}
-      </ReviewContainer>
-    </Main>
+    <CartEventProvider>
+      <Head>
+        <title>
+          {product?.metaTagDescription
+            ? `${product?.metaTagDescription} | Easy.cl - Easy`
+            : 'Easy.cl'}
+        </title>
+      </Head>
+      <Main>
+        <PdpBreadcrumbs />
+        <Section>
+          <ImagesContainer>
+            <ImageGallery />
+          </ImagesContainer>
+          <Separator />
+          <DetailsContainer>
+            <ProductDetails />
+          </DetailsContainer>
+        </Section>
+        <EmotionalDescription />
+        <Section id="specifications">
+          <SpecificationsTables />
+        </Section>
+        <ReviewContainer>
+          {product && <RatingAndReview productInfo={product} />}
+        </ReviewContainer>
+      </Main>
+    </CartEventProvider>
   );
 };
 
