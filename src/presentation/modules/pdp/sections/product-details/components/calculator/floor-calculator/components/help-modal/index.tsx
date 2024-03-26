84ec5d0127aafd48d58fe60abe03c6f63f40d6b2
pbourlot <paula.bourlot@externos-ar.cencosud.com>
2024-03-26 14:14:01 -0300

    fix: floor and paint calculator

--- src/presentation/modules/pdp/sections/product-details/components/calculator/floor-calculator/components/help-modal/index.tsx
+++ src/presentation/modules/pdp/sections/product-details/components/calculator/floor-calculator/components/help-modal/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
   ModalOverlay,
   ModalContent,
@@ -6,6 +6,7 @@ import {
   Title,
   Content,
   TitleModal,
+  BackgroundModal,
 } from './styles';
 import Image from 'next/image';
 import { MdOutlineArrowBackIos } from 'react-icons/md';
@@ -16,53 +17,70 @@ type ModalProps = {
 };
 
 const HelpModal = ({ isOpen, onClose }: ModalProps) => {
-  if (!isOpen) return null;
+  const modalRef = useRef<HTMLDivElement>(null);
 
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (modalRef.current && !modalRef?.current?.contains(e.target as Node)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
+  if (!isOpen) return null;
   return (
-    <ModalOverlay>
-      <ModalContent>
-        <TitleModal>
-          <GoBackButton onClick={onClose}>
-            <MdOutlineArrowBackIos size={'24px'} />
-          </GoBackButton>
-          ¿Cómo calcular los m2 que necesito comprar?
-        </TitleModal>
-        <Title>Superficie simple</Title>
-        <Content>
-          Son los espacios cuadrados o rectangulares. Para obtener la cantidad
-          de m2 que necesitas, debes multiplicar el largo y ancho de la
-          superficie.
-        </Content>
-        <Image
-          src={'/images/floor-calculator/simple.svg'}
-          alt="Superficie simple"
-          width={330}
-          height={170}
-        />
-        <Title>Superficie compuesta</Title>
-        <Content>
-          Son los espacios compuestos por dos o más figuras. Para calcular la
-          cantidad de m2 totales, divide la superficie en figuras simples
-          (cuadrados y rectángulos); multiplica el largo y ancho de cada una; y
-          suma los totales. Si tienes una superficie triangular, multiplica
-          largo y ancho y luego divídelo en dos.
-        </Content>
-        <Image
-          src={'/images/floor-calculator/compose.svg'}
-          alt="Superficie compuesta"
-          width={330}
-          height={250}
-        />
-        <Title>¿Por qué se recomienda agregar un 10% adicional?</Title>
-        <Content style={{ marginBottom: '16px' }}>
-          Las medidas del producto no coinciden con las dimensiones de la
-          superficie donde se aplicará. Esto produce cortes y ajustes que
-          provocan una ligera merma. Te pedimos que agregues un 10% adicional al
-          total de m2 para que puedas terminar tu trabajo sin que te falte
-          material.
-        </Content>
-      </ModalContent>
-    </ModalOverlay>
+    <>
+      <BackgroundModal />
+      <ModalOverlay ref={modalRef}>
+        <ModalContent>
+          <TitleModal>
+            <GoBackButton onClick={onClose}>
+              <MdOutlineArrowBackIos size={'24px'} />
+            </GoBackButton>
+            ¿Cómo calcular los m2 que necesito comprar?
+          </TitleModal>
+          <Title>Superficie simple</Title>
+          <Content>
+            Son los espacios cuadrados o rectangulares. Para obtener la cantidad
+            de m2 que necesitas, debes multiplicar el largo y ancho de la
+            superficie.
+          </Content>
+          <Image
+            src={'/images/floor-calculator/simple.svg'}
+            alt="Superficie simple"
+            width={330}
+            height={170}
+          />
+          <Title>Superficie compuesta</Title>
+          <Content>
+            Son los espacios compuestos por dos o más figuras. Para calcular la
+            cantidad de m2 totales, divide la superficie en figuras simples
+            (cuadrados y rectángulos); multiplica el largo y ancho de cada una;
+            y suma los totales. Si tienes una superficie triangular, multiplica
+            largo y ancho y luego divídelo en dos.
+          </Content>
+          <Image
+            src={'/images/floor-calculator/compose.svg'}
+            alt="Superficie compuesta"
+            width={330}
+            height={250}
+          />
+          <Title>¿Por qué se recomienda agregar un 10% adicional?</Title>
+          <Content style={{ marginBottom: '16px' }}>
+            Las medidas del producto no coinciden con las dimensiones de la
+            superficie donde se aplicará. Esto produce cortes y ajustes que
+            provocan una ligera merma. Te pedimos que agregues un 10% adicional
+            al total de m2 para que puedas terminar tu trabajo sin que te falte
+            material.
+          </Content>
+        </ModalContent>
+      </ModalOverlay>
+    </>
   );
 };
 
--- src/presentation/modules/pdp/sections/product-details/components/calculator/paint-calculator/components/help-modal/index.tsx
+++ src/presentation/modules/pdp/sections/product-details/components/calculator/paint-calculator/components/help-modal/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
   ModalOverlay,
   ModalContent,
@@ -6,6 +6,7 @@ import {
   Title,
   Content,
   TitleModal,
+  BackgroundModal,
 } from './styles';
 import Image from 'next/image';
 import { MdOutlineArrowBackIos } from 'react-icons/md';
@@ -16,47 +17,64 @@ type ModalProps = {
 };
 
 const HelpModal = ({ isOpen, onClose }: ModalProps) => {
-  if (!isOpen) return null;
+  const modalRef = useRef<HTMLDivElement>(null);
 
+  const handleOutsideClick = (e: MouseEvent) => {
+    if (modalRef.current && !modalRef?.current?.contains(e.target as Node)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
+  if (!isOpen) return null;
   return (
-    <ModalOverlay>
-      <ModalContent>
-        <TitleModal>
-          <GoBackButton onClick={onClose}>
-            <MdOutlineArrowBackIos size={'24px'} />
-          </GoBackButton>
-          ¿Cómo calcular los m2 que necesito comprar?
-        </TitleModal>
-        <Title>Largo lineal</Title>
-        <Content>
-          Es la suma del largo de todas las paredes que quieres pintar. Esto te
-          permitirá conocer el largo total de la superficie a pintar.
-        </Content>
-        <Image
-          src={'/images/paint-calculator/simple.svg'}
-          alt="Largo lineal"
-          width={330}
-          height={170}
-        />
-        <Title>Superficie</Title>
-        <Content>
-          Es el resultado de la multiplicación del largo lineal con la altura
-          del espacio.
-        </Content>
-        <Image
-          src={'/images/paint-calculator/compose.svg'}
-          alt="Superficie"
-          width={330}
-          height={250}
-        />
-        <Title>¿Por qué se recomienda agregar un 10% adicional?</Title>
-        <Content style={{ marginBottom: '16px' }}>
-          Dependiendo del producto usado, su aplicación y la superficie, esto
-          podría diferir de lo recomendado, es por esto que se sugiere comprar
-          un poco más para que no te falte pintura.
-        </Content>
-      </ModalContent>
-    </ModalOverlay>
+    <>
+      <BackgroundModal />
+      <ModalOverlay id="modal-help-calculator" ref={modalRef}>
+        <ModalContent>
+          <TitleModal>
+            <GoBackButton onClick={onClose}>
+              <MdOutlineArrowBackIos size={'24px'} />
+            </GoBackButton>
+            ¿Cómo calcular los m2 que necesito comprar?
+          </TitleModal>
+          <Title>Largo lineal</Title>
+          <Content>
+            Es la suma del largo de todas las paredes que quieres pintar. Esto
+            te permitirá conocer el largo total de la superficie a pintar.
+          </Content>
+          <Image
+            src={'/images/paint-calculator/simple.svg'}
+            alt="Largo lineal"
+            width={330}
+            height={170}
+          />
+          <Title>Superficie</Title>
+          <Content>
+            Es el resultado de la multiplicación del largo lineal con la altura
+            del espacio.
+          </Content>
+          <Image
+            src={'/images/paint-calculator/compose.svg'}
+            alt="Superficie"
+            width={330}
+            height={250}
+          />
+          <Title>¿Por qué se recomienda agregar un 10% adicional?</Title>
+          <Content style={{ marginBottom: '16px' }}>
+            Dependiendo del producto usado, su aplicación y la superficie, esto
+            podría diferir de lo recomendado, es por esto que se sugiere comprar
+            un poco más para que no te falte pintura.
+          </Content>
+        </ModalContent>
+      </ModalOverlay>
+    </>
   );
 };
 
